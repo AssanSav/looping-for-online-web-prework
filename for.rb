1b945861697fe522db97202c8122f34adf4dd021
assane savadogo <assane.savadogo81@gmail.com>
2019-05-31 04:23:41 +0000

    Done.

--- for.rb
+++ for.rb
@@ -1,7 +1,19 @@
+def using_for
+for checklist in 1..10 do
+  puts "Wingardium Leviosa"
+   end
+end
+
+
+
+
+
 
 def using_for
 	checklist = 1..10
-	#your code here
+	for using_for in checklist  do 
+	  puts "Wingardium Leviosa"
+	end
 end
 
 
